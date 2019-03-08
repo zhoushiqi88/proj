11ca73c97a4d2974eb26dcbcab46796a643207c2
zhoushiqi88 <zhoushiqi_uestc@yahoo.com>
2019-03-08 21:33:33 +0800

    2

---
+++ testsocket/testsocket/XTcp.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include<string>
+class XTcp
+{
+public:
+	XTcp();
+	int CreatSocket();
+	bool Bind(unsigned short port);
+	XTcp Accept();
+	void Close();
+	int Recv(char*buf,int bufsize);
+	int Send(const char*buf,int bufsize);
+	virtual ~XTcp();
+	int sock = 0;
+	unsigned short port = 0;
+	std::string ip;
+};
+
--- testsocket/testsocket/main.cpp
+++ testsocket/testsocket/main.cpp
@@ -2,22 +2,9 @@
 //
 
 #include "pch.h"
-#include<stdio.h>
-#include <iostream>
-#ifdef WIN32
-#include<Windows.h>
-#else
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<unistd.h>
+#include"XTcp.h"
 #include<stdlib.h>
-#include<arpa/inet.h>
-#include<string.h>
-#define closesocket close
-#endif
-
 #include<thread>
-using namespace std;
 
 class TcpThread{
 public:
@@ -25,7 +12,7 @@ public:
 	{
 		char buf[1024] = {0};
 		for(;;) {
-			int relen = recv(client,buf,sizeof(buf)-1,0);
+			int relen = client.Recv(buf,sizeof(buf)-1);
 			if(relen <= 0) {
 				break;
 			}
@@ -34,59 +21,34 @@ public:
 				break;
 			}
 			buf[relen] = '\0';
-			send(client,"ok\n",3,0);
+			client.Send("ok\n",3);
 			printf("recv %s\n",buf);
 		}
-		closesocket(client);
+		client.Close();
 		delete this;
 	}
-	int client = 0;
+	XTcp client;
 };	
 
 
 int main(int argc,char* argv[])
 {
-#ifdef WIN32
-	WSADATA ws;
-	WSAStartup(MAKEWORD(2, 2), &ws);
-#endif
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == -1) {
-		printf("create socket failed\n");
-		return -1;
-	}
+
 	unsigned short port = 8080;
 	if (argc > 1) {
 		port = atoi(argv[1]);
 	}
-	sockaddr_in saddr;
-	saddr.sin_family = AF_INET;
-	saddr.sin_port = htons(port);
-	saddr.sin_addr.s_addr = htonl(0);
-
-
-	if (bind(sock, (sockaddr*)&saddr, sizeof(saddr)) != 0) {
-		printf("bind port %d failed!\n", port);
-		return -2;
-	}
-	printf("bind port %d success\n", port);
-	listen(sock, 10);
-
-	for(;;) {
-		sockaddr_in caddr;
-		socklen_t len = sizeof(caddr);
-		int client = accept(sock,(sockaddr*)&caddr,&len);
-		if(client <=0) break;
-		printf("accept client %d\n", client);
-		char* ip= inet_ntoa(caddr.sin_addr);
-		unsigned short cport = ntohs(caddr.sin_port);
-		printf("client ip %s\nclient port is %d\n",ip,cport);
+	XTcp ser;
+	ser.CreatSocket();
+	ser.Bind(port);
+	for (;;) {
+		XTcp client = ser.Accept();
 		TcpThread *th = new TcpThread();
 		th->client = client;
-		thread sth(&TcpThread::Main,th);
+		std::thread sth(&TcpThread::Main, th);
 		sth.detach();
 	}
-	closesocket(sock);
+	ser.Close();
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
